31de031628337a9ef05eb3e7037fb61c523ea800
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-07 11:27:13 +0100

    Fix Game Scene into a prototype scene for AB Testing
    Change the way the camera follow works, change the way the player movement works

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private float smoothing;
-    [SerializeField] private Vector3 offset;
+	[SerializeField] private Transform target;
+	[SerializeField] private float smoothing;
+	[SerializeField] private Vector3 offset;
+	[SerializeField] private Vector3 velocity;
+	[Space]
+	[SerializeField] private float clampX = 3.75f;
 
-    void FixedUpdate()
-    {
-        Vector3 desiredLocation = target.position;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredLocation, smoothing);
-        transform.position = smoothedPosition + offset;
-    }
+	void FixedUpdate()
+	{
+		Vector3 desiredLocation = new Vector3(Mathf.Clamp(target.position.x - offset.x, -clampX, clampX), target.position.y - offset.y, offset.z);
+		transform.position = Vector3.SmoothDamp(transform.position, desiredLocation, ref velocity, smoothing);
+	}
 }
--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class PlayerMovementBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 2.5f;
-    [SerializeField]
-    private float maxSpeed = 2;
-    private Rigidbody2D rb2d;
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
+	[SerializeField]
+	private float moveSpeed = 2.5f;
+	[SerializeField]
+	private float maxSpeed = 2;
+	private Rigidbody2D rb2d;
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
 
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        MovePlayer(horizontal);
-    }
+	void Update()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		MovePlayer(horizontal);
+	}
 
-    public void MovePlayer(float direction)
-    {
-        Vector2 move = new Vector2(direction, 0);
-        //transform.position += move * Time.deltaTime * moveSpeed;
-        if (rb2d.velocity.x < maxSpeed && rb2d.velocity.x > -maxSpeed) {
-            rb2d.AddForce(move);
-        }
-    }
+	public void MovePlayer(float direction)
+	{
+		Vector2 move = new Vector2(direction, 0);
+		//transform.Translate(move * Time.deltaTime * moveSpeed);
+		if(rb2d.velocity.x < maxSpeed && rb2d.velocity.x > -maxSpeed)
+		{
+			rb2d.AddForce(move);
+		}
+	}
 }
