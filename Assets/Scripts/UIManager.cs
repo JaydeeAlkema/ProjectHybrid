55e62e236f2a76293aac818fcc4600c1391d4348
joris <j_d_derks@hotmail.com>
2021-01-13 15:44:10 +0100

    add correct background & extra UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Image hungerbar;
+    [SerializeField] private Image thirstBar;
+    [SerializeField] private Image energyBar;
+    [SerializeField] private Image hygieneBar;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
