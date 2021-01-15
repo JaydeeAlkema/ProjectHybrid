9362b29f54b87c15a66f942ec176e6051eb3603b
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-01-15 13:29:13 +0100

    Add 1 minute to the notifications. Mainly for trailer purposes

--- Assets/Scripts/VitalsBehaviour.cs
+++ Assets/Scripts/VitalsBehaviour.cs
@@ -47,12 +47,12 @@ public class VitalsBehaviour : MonoBehaviour
 		if(Random.Range(1, 100) < hungerDecayAmount && hunger > 0)
 		{
 			hunger--;
-			if(hunger < 25) NotificationsManager.SendNotification("1", "Pet Hunger Low!", "Your pet has run very low on food! Give your pet some food before it terrorizes your home!", System.DateTime.Now, default, default);
+			if(hunger < 25) NotificationsManager.SendNotification("1", "Pet Hunger Low!", "Your pet has run very low on food! Give your pet some food before it terrorizes your home!", System.DateTime.Now.AddMinutes(1), default, default);
 		}
 		if(Random.Range(1, 100) < thirstDecayAmount && thirst > 0)
 		{
 			thirst--;
-			if(thirst < 25) NotificationsManager.SendNotification("1", "Pet Thirst Low!", "Your pet has run very low on Water! Give your pet some Water before it terrorizes your home!", System.DateTime.Now, default, default);
+			if(thirst < 25) NotificationsManager.SendNotification("1", "Pet Thirst Low!", "Your pet has run very low on Water! Give your pet some Water before it terrorizes your home!", System.DateTime.Now.AddMinutes(1), default, default);
 		}
 		if(Random.Range(1, 100) < energyDecayAmount && energy > 0)
 		{
@@ -61,14 +61,14 @@ public class VitalsBehaviour : MonoBehaviour
 				fireEvent.IsActive = true;
 				energy = 100;
 
-				NotificationsManager.SendNotification("1", "Pet Energy Low!", "Your pet has run very low on energy!", System.DateTime.Now, default, default);
+				NotificationsManager.SendNotification("1", "Pet Energy Low!", "Your pet has run very low on energy!", System.DateTime.Now.AddMinutes(1), default, default);
 			}
 			energy--;
 		}
 		if(Random.Range(1, 100) < hygieneDecayAmount && hygiene > 0)
 		{
 			hygiene--;
-			if(thirst < 25) NotificationsManager.SendNotification("1", "Pet Hygiene Low!", "Your pet is very filthy! Give him a wash or suffer the consequences!", System.DateTime.Now, default, default);
+			if(thirst < 25) NotificationsManager.SendNotification("1", "Pet Hygiene Low!", "Your pet is very filthy! Give him a wash or suffer the consequences!", System.DateTime.Now.AddMinutes(1), default, default);
 
 		}
 	}
