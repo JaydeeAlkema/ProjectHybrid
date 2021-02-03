1f50027bab0f841ccd9563c73fb59e86b9727b15
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2021-02-03 13:36:15 +0100

    Add Routine Class

---
+++ Assets/Scripts/New Concept/Routine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Routine : MonoBehaviour
+{
+	[SerializeField] private new string name = default;
+	[SerializeField] private string description = default;
+	[SerializeField] private DateTime dateTime = default;
+	[SerializeField] private DateTime timeTillRoutine = default;
+
+	public string Name { get => name; set => name = value; }
+	public string Description { get => description; set => description = value; }
+	public DateTime DateTime { get => dateTime; set => dateTime = value; }
+	public DateTime TimeTillRoutine { get => timeTillRoutine; set => timeTillRoutine = value; }
+}
