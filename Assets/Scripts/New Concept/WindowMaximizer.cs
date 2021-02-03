e94cbc5008d9a0a8a829db70dce3d8379327c3af
joris <j_d_derks@hotmail.com>
2021-02-03 19:51:15 +0100

    add button rotat E

---
+++ Assets/Scripts/New Concept/WindowMaximizer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowMaximizer : MonoBehaviour
+{
+    private Animator anim;
+    [SerializeField]
+    private Animator buttonAnimator;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isUp", false);
+        buttonAnimator.SetBool("isButtonUp", true);
+    }
+
+
+
+
+    public void ToggleWindow()
+    {
+        if (anim.GetBool("isUp") == true)
+        {
+            anim.SetBool("isUp", false);
+        } 
+        else
+        {
+            anim.SetBool("isUp", true);
+        }
+    }
+
+    public void FlipButton() 
+    {
+        if (buttonAnimator.GetBool("isButtonUp") == false)
+        {
+            buttonAnimator.SetBool("isButtonUp", true);
+        }
+        else
+        {
+            buttonAnimator.SetBool("isButtonUp", false);
+        }
+    }
+
+}
